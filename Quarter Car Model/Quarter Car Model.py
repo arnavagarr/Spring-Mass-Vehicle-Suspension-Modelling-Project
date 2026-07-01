196e0b1801916cabf55433bc063b4f2c87e2ee88
Arnav A <arnav2007.agarwalla@gmail.com>
2026-07-01 17:09:43 +0400

    Update Quarter Car Model.py

--- Quarter Car Model/Quarter Car Model.py
+++ Quarter Car Model/Quarter Car Model.py
@@ -9,7 +9,7 @@ cs = 1000
 kt = 150000
 ks = 15000
 t_span = (0, 10)
-y0 = [0.05, 0, 0, 0]
+y0 = [5, 2, 3, 2]
 
 #road position func
 def r(t):
@@ -24,9 +24,9 @@ def quarter_car(t, state):
     x4 = state[3] #wheel vel
     
     dx1dt = x2 
-    dx2dt =((1/ms)*(ks*(x3 - x1) - cs*(x4 - x2)))
+    dx2dt =(-ks*(x1 - x3) - cs*(x2 - x4)) / ms
     dx3dt = x4
-    dx4dt = ((-ks*(x3 - x1) - cs*(x4 - x2) + kt*(r(t) - x3))*(1/mu))
+    dx4dt = ( ks*(x1 - x3) + cs*(x2 - x4) - kt*(x3 - r(t)) ) / mu
 
     return [dx1dt,dx2dt,dx3dt,dx4dt]
 
@@ -40,36 +40,27 @@ road = r(solution.t)
 #plot section
 
 figure , axis = plt.subplots(1, 3)
-#postion -graph 0
-axis[0].plot(solution.t, solution.y[0], label='Body' ,color ='red')
-axis[0].plot(solution.t, solution.y[2], label='Wheel' ,color ='green')
+
+axis[0].plot(solution.t, solution.y[0], label='Body(sprung)' ,color ='red')
+axis[0].plot(solution.t, solution.y[2], label='Wheel(sprung)' ,color ='green')
 axis[0].legend()
-
-axis[0].set_xlabel("Time/(s)")
-axis[0].set_ylabel("Position")
-
-axis[0].set_title("Position against Time")
-axis[0].grid(True)
-
-#velocity -graph 1
-axis[1].plot(solution.t, solution.y[1], label='Body' ,color ='blue')
-axis[1].plot(solution.t, solution.y[3], label='Wheel' ,color ='orange')
+axis[1].plot(solution.t, solution.y[1], label='Body(unsprung)' ,color ='blue')
+axis[1].plot(solution.t, solution.y[3], label='Wheel(unsprung)' ,color ='orange')
 axis[1].legend()
-
-axis[1].set_xlabel("Time/(s)")
-axis[1].set_ylabel("Velociity")
-
-axis[1].set_title("Velocity against Time")
-axis[1].grid(True)
-
-#road postion -graph 2
 axis[2].plot(solution.t, road, label='Road Position' ,color ='black')
 axis[2].legend()
+
+axis[0].set_xlabel("Time/(s)")
+axis[0].set_ylabel("Position/Velocity(Sprung)")
+
+axis[1].set_xlabel("Time/(s)")
+axis[1].set_ylabel("Position/Velocity(Unsprung)")
 
 axis[2].set_xlabel("Time/(s)")
 axis[2].set_ylabel("Road Position")
 
-axis[2].set_title("Road Position against Time")
-axis[2].grid(True)
+axis[0].set_xlabel("Time/(s)")
+axis[0].set_ylabel("Position")
+
 
 plt.show()
